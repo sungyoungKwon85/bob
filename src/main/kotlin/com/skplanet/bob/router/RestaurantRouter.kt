460ae4ec26dbbcb32abb29fbca3b16b8da4bd9d4
권성영(Sungyoung Kwon)/AI/Mobility개발팀/SKP <kkwonsy@sk.com>
2020-02-13 18:42:51 +0900

    register route as Bean

--- src/main/kotlin/com/skplanet/bob/router/RestaurantRouter.kt
+++ src/main/kotlin/com/skplanet/bob/router/RestaurantRouter.kt
@@ -1,12 +1,14 @@
 package com.skplanet.bob.router
 
 import com.skplanet.bob.handler.RestaurantHandler
+import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.router
 
 @Component
 class RestaurantRouter(private val restaurantHandler: RestaurantHandler) {
 
+    @Bean
     fun restaurantRoutes() = router {
         "/restaurant".nest {
             GET("/{id}", restaurantHandler::get)
