29c52f823f4107a10f331c4cdcdf68bb53cd1fd4
sungyoungKwon85 <ssyang0@naver.com>
2020-11-03 17:26:20 +0900

    로그추가, 범위별포인트조회 로직 변경

--- src/main/kotlin/com/skplanet/bob/api/model/PointsResponse.kt
+++ src/main/kotlin/com/skplanet/bob/api/model/PointsResponse.kt
@@ -1,12 +1,13 @@
 package com.skplanet.bob.api.model
 
 data class PointsResponse(
-        val points: MutableList<Point> = ArrayList(),
-        var totalCount: Long? = 0
+        var points: MutableList<Point> = ArrayList(),
+        var totalCount: Long = 0
 ) {
     data class Point(
             var latitude: Double? = 0.0,
             var longitude: Double? = 0.0,
-            var count: Long? = 0
+            var count: Long = 0,
+            var ids: List<String> = ArrayList()
     )
 }
