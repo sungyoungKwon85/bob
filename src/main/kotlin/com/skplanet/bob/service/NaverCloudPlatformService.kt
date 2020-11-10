26174eb5f131180b0af323a26679ee99c0c02e94
sungyoungKwon85 <ssyang0@naver.com>
2020-11-10 14:15:08 +0900

    - reverse geocode 추가 - point Response에 센터 정보 추가

--- src/main/kotlin/com/skplanet/bob/service/NaverCloudPlatformService.kt
+++ src/main/kotlin/com/skplanet/bob/service/NaverCloudPlatformService.kt
@@ -1,18 +1,15 @@
 package com.skplanet.bob.service
 
 import com.skplanet.bob.api.model.NaverGeocodeResponse
+import com.skplanet.bob.api.model.NaverReverseGeocodeResponse
 import com.skplanet.bob.config.NaverProperties
-import kotlinx.coroutines.reactive.awaitFirst
 import lombok.extern.slf4j.Slf4j
 import org.springframework.http.HttpHeaders
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.stereotype.Service
-import org.springframework.web.reactive.function.client.ClientResponse
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 import reactor.netty.http.client.HttpClient
-import java.time.Duration
 
 @Slf4j
 @Service
@@ -21,14 +18,22 @@ class NaverCloudPlatformService(
         val builder: WebClient.Builder
 ) {
 
-    suspend fun geocode(query: String): NaverGeocodeResponse? {
-        return builder.baseUrl("https://naveropenapi.apigw.ntruss.com")
+    companion object {
+        const val GEO_URL = "https://naveropenapi.apigw.ntruss.com"
+    }
+
+    init {
+        builder.baseUrl(GEO_URL)
                 .defaultHeader("X-NCP-APIGW-API-KEY-ID", naverProperties.clientId)
                 .defaultHeader("X-NCP-APIGW-API-KEY", naverProperties.clientSecret)
                 .defaultHeader(HttpHeaders.ACCEPT, "application/json")
                 .clientConnector(ReactorClientHttpConnector(
                         HttpClient.create().wiretap(true)
                 ))
+    }
+
+    fun geocode(query: String): Mono<NaverGeocodeResponse> {
+        return builder
                 .build().get()
                 .uri { uriBuilder ->
                     uriBuilder
@@ -38,7 +43,21 @@ class NaverCloudPlatformService(
                 }
                 .retrieve()
                 .bodyToMono(NaverGeocodeResponse::class.java)
-                .awaitFirst()
+    }
 
+    fun reverseGeocode(lon: Double, lat: Double): Mono<NaverReverseGeocodeResponse> {
+        val coords = "$lon,$lat"
+        return builder
+                .build().get()
+                .uri { uriBuilder ->
+                    uriBuilder
+                            .path("/map-reversegeocode/v2/gc")
+                            .queryParam("output", "json")
+                            .queryParam("orders", "admcode")
+                            .queryParam("coords", coords)
+                            .build()
+                }
+                .retrieve()
+                .bodyToMono(NaverReverseGeocodeResponse::class.java)
     }
 }
